1d8cf0e7a12b3dad19c8c3e73ddbb4315bd9c6a8
Ale <alejandra022m@gmail.com>
2026-07-20 11:41:05 -0500

    Update Grupo2_CajeroAutomatico.java
    1 borrador

--- src/grupo2_cajeroautomatico/Grupo2_CajeroAutomatico.java
+++ src/grupo2_cajeroautomatico/Grupo2_CajeroAutomatico.java
@@ -11,6 +11,45 @@ package grupo2_cajeroautomatico;
 public class Grupo2_CajeroAutomatico {
 
     public static void main(String[] args) {
+                
+        int opci;
+        
+        int saldo =1000000;
+        
+        System.out.println("Opcion 1: Mostrar saldo actual");
+        System.out.println("Opcion 2: Retirar ");
+        System.out.println("Opcion 3: Consignar");
+        System.out.println("Opcion 4: Consultar Historial");
+        System.out.println("Opcion 5: Salir");
+        opci = sc.nextInt();
+        
+        
+        switch (opci) {
+            case 1:
+                System.out.println("Su saldo actual es de : " + saldo);
+                break;
+                
+            case 2:
+                System.out.println("Ingrse saldo a retirar: ");
+                int salretirar = sc.nextInt();
+                
+                if (salretirar>saldo) {System.out.println("Fondos insuficientes");
+                    
+                } else {
+                    int retiro = saldo-salretirar;
+                    System.out.println("Su saldo actual es de:" + retiro );
+                }
+                
+            case 3:
+                System.out.println("Ingrese el numero de su cuenta: ");
+                
+                
+
+                
+                
+            default:
+
+        }
 
     }
     
